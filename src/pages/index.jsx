5f4bf2367153197488578ae68a83c6ee69889e26
hahayz77 <danilo__danilo@hotmail.com>
2023-06-15 15:28:25 -0300

    localStorage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,26 +1,34 @@
 import { Inter } from 'next/font/google'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
+    const [darkMode, setDarkMode] = useState(); //boolean value
 
-  const [darkMode, setDarkMode] = useState(false);
+    useEffect(() => {
+        const storedDarkMode = JSON.parse(localStorage.getItem('darkMode'));
+        if (storedDarkMode) {
+            document.querySelector('html').classList.add('dark');
+            setDarkMode(storedDarkMode);
+        }
+    }, []);
 
-  const toggleDarkMode = () => {
-    setDarkMode(prevDarkMode => !prevDarkMode);
-    document.querySelector("html").classList.toggle("dark");
-  };
+    const toggleDarkMode = () => {
+        document.querySelector('html').classList.toggle('dark', !darkMode);
+        localStorage.setItem('darkMode', JSON.stringify(!darkMode));
+        setDarkMode(!darkMode);
+    };
 
-  return (
-    <>
-      <h1>Hello world!</h1>
-      <h2 className='text-slate-500 dark:text-slate-500'>Click to toggle the Theme</h2>
-      <button className='bg-slate-300 rounded-full text-white p-2 h-12 w-12 block'
-        onClick={toggleDarkMode}>
-        {darkMode ? "🌚" : "🌞"}
-      </button>
-    </>
-  )
+    return (
+        <main>
+            <h1>Hello world!</h1>
+            <button className='bg-slate-700 dark:bg-slate-300 rounded-full text-white place-self-center p-2 my-3 h-24 w-24 text-3xl'
+                onClick={toggleDarkMode}>
+                {darkMode ? "🌚" : "🌞"}
+            </button>
+            <h2 className='text-slate-500 dark:text-slate-500'>Click to toggle the Theme</h2>
+        </main>
+    )
 }
